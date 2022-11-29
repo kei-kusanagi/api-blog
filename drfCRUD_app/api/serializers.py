061f3e423ad6077c4f868fe00e952a56a72664b3
kei-kusanagi <kei_kusanagi@outlook.com>
2022-11-29 14:00:31 -0600

    fix serializers

--- drfCRUD_app/api/serializers.py
+++ drfCRUD_app/api/serializers.py
@@ -2,8 +2,10 @@ from rest_framework import serializers
 from drfCRUD_app import models
 
 class BlogSerializer(serializers.ModelSerializer):
+    review_user = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = models.Blog
-        fields = ('id', 'titulo', 'entrada', 'fecha')
-        read_only_fields = ('fecha',)
-        review_user = serializers.StringRelatedField(read_only=True)
+        # fields = ('id', 'titulo', 'entrada', 'fecha', 'review_user',)
+        fields = "__all__"
+        read_only_fields = ('fecha','review_user',)
+        
